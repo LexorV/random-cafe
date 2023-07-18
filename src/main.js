9ad98bee055568e5620302ee78cb63842ecaf1ac
Lex <Lexor.V.A.S@yandex.ru>
2023-07-18 21:29:42 +0300

    add route&store&ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import { createRouter, createWebHistory } from 'vue-router'
+import 'element-plus/dist/index.css'
+import ru from 'element-plus/dist/locale/ru.mjs'
 import App from './App.vue'
+import store from './store'
+import CafeFeed from '@/components/CafeFeed'
 
-createApp(App).mount('#app')
+
+
+
+const router = createRouter({
+    routes: [{
+        path: '/', component: CafeFeed,
+    },
+],
+    history: createWebHistory()
+  })
+const app = createApp(App);
+app.use(ElementPlus, {
+    locale: ru,
+  })
+app.use(router)
+app.use(store)
+app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+export const store = createStore({
+  state () {
+    return {
+      cafe: [],
+    }
+  },
+  mutations: {
+  },
+  getters: {
+  }
+})
+export default store
