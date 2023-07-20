360a79f62bb1b63fdef086d382b4a731c3f378b9
Lex <Lexor.V.A.S@yandex.ru>
2023-07-20 23:27:35 +0300

    delete console

--- src/page/OneCafe.vue
+++ src/page/OneCafe.vue
@@ -11,8 +11,6 @@ export default {
     CafeCard
   },
  mounted () {
-    console.log(process.env.VUE_APP_BACKEND_URL)
-    console.log(this.$route.params.id)
    this.$store.dispatch('fetchOneCafe', this.$route.params.id )
   }
 }
